d2bb72b3d1373c438e68dc920eeb0ab8fb6aa0c1
NoorMRaza <noor.m.raza@gmail.com>
2024-06-06 15:53:02 +0500

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
